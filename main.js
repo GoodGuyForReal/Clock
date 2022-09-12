5d7d0e0d25e19469a5a965b41d883b3bae41a267
Tarik Doganay <99910240+GoodGuyForReal@users.noreply.github.com>
2022-09-12 21:27:48 +0200

    saat

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ function Updateclock() {
     let saat = time.getHours();
     let dakika = time.getMinutes();
     let saniye = time.getSeconds();
-    let twelve = 12 - saat;
+    let twelve = saat - 12;
 
 
 
